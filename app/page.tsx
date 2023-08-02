095e7276181582ee2ca39626c4eefe84310127d9
YoussefNesafe <ynessafe@gmail.com>
2023-08-03 01:46:00 +0400

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { CustomButton } from '@/components/ui/CustomButton'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <CustomButton variant={'outline'} size={'lg'}> Test </CustomButton>
+    </main>
+  )
+}
